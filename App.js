4e6dcbd60db65c234a05bd3b59359c235d34b549
KumarRounik <89687049+KumarRounik@users.noreply.github.com>
2023-09-23 02:04:40 +0530

    Heading Red

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const heading1=React.createElement(
+const heading = React.createElement(
     "h1",
     {
     id: "title",
@@ -6,27 +6,26 @@ const heading1=React.createElement(
     "Heading 1"
 );
 
-
 const heading2 = React.createElement(
-            "h1",
-            {
-                id: "title",
-            },
-            "Heading 2"
+    "h2",
+    {
+        id: "title",
+    },
+    "Heading 2"
 );
 
 const container = React.createElement(
-            "div",
-            {
-                id: "container",
-            },
-            [heading, heading2]
+    "div",
+    {
+        id: "container",
+    },
+    [heading, heading2]
 );
 
-        console.log(heading);
+console.log(heading);
 
-        const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-        //passing a react element inside the root
+ //passing a react element inside the root
 
-        root.render(container);
+root.render(container); 
