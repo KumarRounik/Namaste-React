691b251f98f638565f3d0b2b98dde0fedb714249
KumarRounik <89687049+KumarRounik@users.noreply.github.com>
2023-09-24 14:11:06 +0530

    Parcel

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
+/*
+*
+*   //PARCEL BENEFITS//
+*   Creates a Server
+*   HMR- Hot Module Replacement
+*   File watcher algorithm - C++
+*   Bundling
+*   Minify
+*   cleaning our code
+*   Dev and Production Build
+*   Super Fast Build Algorithm
+*   Image Optimization
+*   Caching while Development
+*   Compression
+*   Compatible with older version of browser
+*   https on dev
+*   Manages Port Number
+*   Consistent Hashing Algorithm
+*   Zero Configuration
+*
+*
+*/
+
+
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
 const heading = React.createElement(
     "h1",
     {
     id: "title",
     },
-    "Heading 1"
+    "Heading 1 for parcel"
 );
 
 const heading2 = React.createElement(
